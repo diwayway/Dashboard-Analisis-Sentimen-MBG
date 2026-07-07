76c05c1a60b0dad2a0549af8a37d83c41fc88aa3
diwayway <aldiab778@gmail.com>
2026-07-07 19:38:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,43 @@ html, body, [class*="css"] {
     margin-top:15px;
 }
 
+.metric-header{
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+}
+
+.tooltip{
+    position:relative;
+    display:inline-block;
+    cursor:pointer;
+    font-size:18px;
+    font-weight:bold;
+    color:white;
+}
+
+.tooltip .tooltiptext{
+    visibility:hidden;
+    width:240px;
+    background:#ffffff;
+    color:#111111;
+    text-align:left;
+    border-radius:12px;
+    padding:12px;
+    position:absolute;
+    z-index:999;
+    top:30px;
+    right:0;
+    border:1px solid #EAEAEA;
+    box-shadow:0px 8px 20px rgba(0,0,0,0.15);
+    font-size:13px;
+    line-height:1.6;
+}
+
+.tooltip:hover .tooltiptext{
+    visibility:visible;
+}
+
 /* INSIGHT BOX */
 .insight-box{
     background:#F8F8F8;
@@ -139,7 +176,7 @@ total_data = len(df)
 netral = len(df[df["sentimen"] == "netral"])
 negatif = len(df[df["sentimen"] == "negatif"])
 positif = len(df[df["sentimen"] == "positif"])
-accuracy = 94.90
+accuracy = 94.69
 
 # =====================================
 # HEADER
@@ -173,20 +210,65 @@ with tab1:
     col1, col2, col3, col4 = st.columns(4)
 
     cards = [
-        ("Total Data", total_data, "card-purple"),
-        ("Netral", netral, "card-green"),
-        ("Negatif", negatif, "card-pink"),
-        ("Positif", positif, "card-blue")
-    ]
+    ("Total Data", total_data, "card-purple", ""),
+    (
+        "Netral",
+        netral,
+        "card-green",
+        "Opini yang tidak secara jelas mendukung maupun menolak Program Makan Bergizi Gratis (MBG)."
+    ),
+    (
+        "Negatif",
+        negatif,
+        "card-pink",
+        "Opini yang berisi kritik, penolakan, atau ketidakpuasan terhadap Program Makan Bergizi Gratis (MBG)."
+    ),
+    (
+        "Positif",
+        positif,
+        "card-blue",
+        "Opini yang mendukung, mengapresiasi, atau memberikan tanggapan positif terhadap Program Makan Bergizi Gratis (MBG)."
+    )
+]
 
-    for col, (title, value, color) in zip(
-        [col1, col2, col3, col4], cards
-    ):
-        with col:
+    for col, (title, value, color, info) in zip(
+    [col1, col2, col3, col4], cards
+):
+    with col:
+
+        if title == "Total Data":
+
             st.markdown(f"""
             <div class="metric-card {color}">
                 <div class="metric-title">{title}</div>
                 <div class="metric-value">{value}</div>
+            </div>
+            """, unsafe_allow_html=True)
+
+        else:
+
+            st.markdown(f"""
+            <div class="metric-card {color}">
+
+                <div class="metric-header">
+
+                    <div class="metric-title">
+                        {title}
+                    </div>
+
+                    <div class="tooltip">
+                        ⓘ
+                        <span class="tooltiptext">
+                        {info}
+                        </span>
+                    </div>
+
+                </div>
+
+                <div class="metric-value">
+                    {value}
+                </div>
+
             </div>
             """, unsafe_allow_html=True)
 
